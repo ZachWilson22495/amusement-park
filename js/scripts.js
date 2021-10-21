bc8e58c3383a6f2861ea115f99ffd8a025af20f3
Zach Wilson <zwilson22495@gmail.com>
2021-10-21 10:49:54 -0700

    Add a list of rides in html and multiple height classes in javascript. Give each ride a height class.
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
   $("#height-form").submit(function() {
-    const heightInput = $("input#height").val();
+    const heightInput = parseInt($("input#height").val());
+
+    if (heightInput <= 44) {
+      $('.short-height').show();
+    } else if (heightInput <= 55) {
+      $('.medium-height').show();
+      $('.short-height').show();
+    } else (heightInput => 44) {
+      $('.medium-height').show();
+      $('.tall-height')
+    }
   });
 });
