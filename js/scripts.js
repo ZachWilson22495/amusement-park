94cc0e1cf6792333ff3c9428128f4008f2589e64
Zach Wilson <zwilson22495@gmail.com>
2021-10-21 11:06:00 -0700

    Allow height entry to provide a list of rides suitable for your height.
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
-  $("#height-form").submit(function() {
+  $("#height-form").submit(function(event) {
+    event.preventDefault();
     const heightInput = parseInt($("input#height").val());
 
     if (heightInput <= 44) {
+      $('.every-height').show();
       $('.short-height').show();
+      $('.medium-height').hide();
+      $('.tall-height').hide();
     } else if (heightInput <= 55) {
       $('.medium-height').show();
       $('.short-height').show();
-    } else (heightInput => 44) {
+      $('.every-height').show();
+      $('.tall-height').hide();
+    } else {
+      $('.every-height').show();
       $('.medium-height').show();
-      $('.tall-height')
+      $('.tall-height').show();
+      $('.short-height').hide();
     }
   });
 });
